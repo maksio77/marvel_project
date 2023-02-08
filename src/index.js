301c734a06150c89bb463c5ef2646efcf0d07591
Maksym Pavliv <maksiopavliv0@gmail.com>
2023-02-08 14:49:03 +0200

    random character

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
-import MarvelService from './components/services/MarvelService';
 import './style/style.scss';
 
-const marvelService = new MarvelService();
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
-
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
